c76dfa62ed72ddd209720f5158e21c5fb3161f38
evanina <eva.degano@gmail.com>
2021-12-04 15:16:53 +0100

    signup hbs

--- ironhack-project2/app.js
+++ ironhack-project2/app.js
@@ -33,4 +33,38 @@ app.use("/auth", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+// Google 
+/*
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_OAUTH_ID,
+      clientSecret: process.env.GOOGLE_OAUTH_SECRET,
+      callbackURL: process.env.GOOGLE_OAUTH_CALLBACK_URL || "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // to see the structure of the data in received response:
+      console.log("Google account details:", profile);
+
+      User.findOne({ googleID: profile.id })
+        .then(user => {
+          if (user) {
+            done(null, user);
+            return;
+          }
+
+          User.create({ googleID: profile.id })
+            .then(newUser => {
+              done(null, newUser);
+            })
+            .catch(err => done(err)); // closes User.create()
+        })
+        .catch(err => done(err)); // closes User.findOne()
+    }
+  )
+);
+*/
+
 module.exports = app;
