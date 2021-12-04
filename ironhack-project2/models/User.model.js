e99a5899da6225dcb083056effff01e081861e94
evanina <eva.degano@gmail.com>
2021-12-04 10:25:32 +0100

    started homepage

--- ironhack-project2/models/User.model.js
+++ ironhack-project2/models/User.model.js
@@ -1,16 +1,29 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+// declare user schema
 const userSchema = new Schema(
   {
-    username: {
+    firstName: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      trim: true,
+      lowercase: true,
+      required: [true, "Plese enter your first name."]
     },
-    password: String,
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: [true, "Please enter your email address."],
+      unique: [true, "This email address is already linked to an account."],
+      match: [/^\s+@\s+\.\s+$/, "Please use a valid email address."]
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password."],
+      match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, "Password must have at least 8 characters and include at least one lowercase and one uppercase letter, one number and one special character."]
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
