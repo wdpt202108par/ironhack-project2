144bad4ceeeacfa15aa4aa54db1c5cbd69d57841
evanina <eva.degano@gmail.com>
2021-12-07 08:53:08 +0100

    added models to routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,10 @@ const mongoose = require("mongoose");
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
 
-// Require the User model in order to interact with the database
+// require models in order to interact with the database
 const User = require("../models/User.model");
+const Goal = require("../models/Goal.model");
+const Task = require("../models/Task.model");
 
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
